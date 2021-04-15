3656fc1e09ead414c4234440cece743deafa249e
ChrisJChang <christopher.chang@uqconnect.edu.au>
2021-04-15 16:46:18 +1000

    Adding in the ATLAS MONOJET 139invfb analysis

--- ColliderBit/src/analyses/AnalysisContainer.cpp
+++ ColliderBit/src/analyses/AnalysisContainer.cpp
@@ -66,6 +66,7 @@ namespace Gambit
       F(ATLAS_13TeV_MultiLEP_2LepPlusJets_36invfb)   \
       F(ATLAS_13TeV_MultiLEP_3Lep_36invfb)           \
       F(ATLAS_13TeV_MultiLEP_strong_139invfb)        \
+      F(ATLAS_13TeV_MONOJET_139infb)                 \
       F(ATLAS_13TeV_2OSLEP_chargino_80invfb)         \
       F(ATLAS_13TeV_2OSLEP_chargino_binned_80invfb)  \
       F(ATLAS_13TeV_2OSLEP_chargino_inclusive_80invfb)  \
