d96bb6f63a2326a2b92d3bba3aa19de88e54f885
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2021-10-25 22:01:36 +0200

    Added backend reference string in the BOSS config file for Rivet, and added a castxml dependency for the Rivet cmake target (to ensure castxml is downloaded if needed).
    	modified:   Backends/include/gambit/Backends/backend_types/Rivet_3_1_4/identification.hpp
    	modified:   Backends/scripts/BOSS/configs/rivet_3_1_4.py
    	modified:   cmake/backends.cmake

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -1757,6 +1757,7 @@ endif()
 check_ditch_status(${name} ${ver} ${dir} ${ditch_if_absent})
 if(NOT ditched_${name}_${ver})
   ExternalProject_Add(${name}_${ver}
+    DEPENDS castxml
     DEPENDS ${yoda_name}
     DEPENDS ${hepmc_name}
     DEPENDS ${fjcontrib_name}_${fjcontrib_ver}
