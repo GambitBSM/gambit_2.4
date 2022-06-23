48b9404a37c3eed0c262aa596f32df85ea4de314
Tomas Gonzalo <tomas.gonzalo@kit.edu>
2022-06-24 00:15:51 +0200

    Finished with hdf5 combination with python script

--- Core/src/depresolver.cpp
+++ Core/src/depresolver.cpp
@@ -2439,7 +2439,7 @@ namespace Gambit
       auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
       std::stringstream ss;
-      ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d");
+      ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %H:%M");
       metadata["Date"] =  ss.str();
 
       // Parameters
