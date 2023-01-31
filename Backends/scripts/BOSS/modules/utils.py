72f4e6ef4d4efe407baba5fc72fb2f6e61ad0f70
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2023-01-31 19:02:12 +0100

    BOSS: Commented out old, unused code that was collecting typedefs.

--- Backends/scripts/BOSS/modules/utils.py
+++ Backends/scripts/BOSS/modules/utils.py
@@ -2661,7 +2661,7 @@ def clearGlobalXMLdicts():
     gb.file_dict.clear()
     gb.std_types_dict.clear()
     gb.std_typedef_names_dict.clear()
-    gb.typedef_dict.clear()
+    # gb.typedef_dict.clear()
     gb.loaded_classes_in_xml.clear()
     gb.func_dict.clear()
 
@@ -2733,35 +2733,37 @@ def initGlobalXMLdicts(xml_path, id_and_name_only=False):
 
 
         # Update global dict: typedef name --> typedef xml element
+        # Collect list of xml elements for all (global) typedefs
         if el.tag == 'Typedef':
 
             # Collect list of xml elements for all (global) typedefs
             if 'access' not in el.keys():
                 all_typedef_elems.append(el)
 
-            # Only accept native typedefs:
-            if isNative(el):
+            # Commented out the block below, as we're currently not using gb.typedef_dict for anything
+            # # Only accept native typedefs:
+            # if isNative(el):
 
-                typedef_name = el.get('name')
+            #     typedef_name = el.get('name')
 
-                type_dict = findType(el)
-                type_el = type_dict['el']
+            #     type_dict = findType(el)
+            #     type_el = type_dict['el']
 
-                # If underlying type is a fundamental or standard type, accept it right away
-                if isFundamental(type_el) or isStdType(type_el):
-                    gb.typedef_dict[typedef_name] = el
+            #     # If underlying type is a fundamental or standard type, accept it right away
+            #     if isFundamental(type_el) or isStdType(type_el):
+            #         gb.typedef_dict[typedef_name] = el
 
-                # If underlying type is a class/struct, check if it's acceptable
-                elif type_el.tag in ['Class', 'Struct']:
+            #     # If underlying type is a class/struct, check if it's acceptable
+            #     elif type_el.tag in ['Class', 'Struct']:
 
-                    type_name = classutils.getClassNameDict(type_el)
+            #         type_name = classutils.getClassNameDict(type_el)
 
-                    if type_name['long_templ'] in cfg.load_classes:
-                        gb.typedef_dict[typedef_name] = el
+            #         if type_name['long_templ'] in cfg.load_classes:
+            #             gb.typedef_dict[typedef_name] = el
 
-                # If neither fundamental or class/struct, ignore it.
-                else:
-                    pass
+            #     # If neither fundamental or class/struct, ignore it.
+            #     else:
+            #         pass
 
 
         # Update global dict: function name --> function xml element
