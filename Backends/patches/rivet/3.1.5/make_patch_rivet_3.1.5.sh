9e58e66b203b305f7733832e671987b390324f83
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2023-02-01 19:29:56 +0100

    Small update to Rivet patch to fix namespace ambiguity that caused clang to complain.

--- Backends/patches/rivet/3.1.5/make_patch_rivet_3.1.5.sh
+++ Backends/patches/rivet/3.1.5/make_patch_rivet_3.1.5.sh
@@ -14,5 +14,7 @@ echo "diff -rupN Rivet-3.1.5/src/Core/AnalysisHandler.cc ../installed/rivet/3.1.
 diff -rupN Rivet-3.1.5/src/Core/AnalysisHandler.cc ../installed/rivet/3.1.5/src/Core/AnalysisHandler.cc >> patch_rivet_3.1.5.dif
 echo "diff -rupN Rivet-3.1.5/analyses/Makefile.in ../installed/rivet/3.1.5/analyses/Makefile.in" >> patch_rivet_3.1.5.dif
 diff -rupN Rivet-3.1.5/analyses/Makefile.in ../installed/rivet/3.1.5/analyses/Makefile.in >> patch_rivet_3.1.5.dif
+echo "diff -rupN Rivet-3.1.5/analyses/pluginCMS/CMS_2018_I1663958.cc ../installed/rivet/3.1.5/analyses/pluginCMS/CMS_2018_I1663958.cc" >> patch_rivet_3.1.5.dif
+diff -rupN Rivet-3.1.5/analyses/pluginCMS/CMS_2018_I1663958.cc ../installed/rivet/3.1.5/analyses/pluginCMS/CMS_2018_I1663958.cc >> patch_rivet_3.1.5.dif
 mv patch_rivet_3.1.5.dif ../patches/rivet/3.1.5/
 cd ../patches/rivet/3.1.5
