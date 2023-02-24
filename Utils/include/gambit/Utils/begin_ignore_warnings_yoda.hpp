bbd9fcf6016380c367b503a8b6977066faa1acaf
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2023-02-24 02:15:32 +0100

    Silenced some compiler warnings from YODA. (Added new header file Utils/include/gambit/Utils/begin_ignore_warnings_yoda.hpp.)

---
+++ Utils/include/gambit/Utils/begin_ignore_warnings_yoda.hpp
@@ -0,0 +1,45 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  Pragma directives to suppress compiler warnings
+///  coming from including YODA headers.
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///
+///  \author Anders Kvellestad
+///          (anders.kvellestad@fys.uio.no)
+///  \date 2023 Feb
+///
+///  *********************************************
+
+
+#include "gambit/cmake/cmake_variables.hpp"
+
+#ifdef SUPPRESS_LIBRARY_WARNINGS
+
+  // GCC:
+  // clang also depfines __GNUC__ so make sure it is only GCC
+  #if defined(__GNUC__) && !defined(__clang__)
+    // Save diagnostic state
+    #pragma GCC diagnostic push 
+    // Don't care if an old compiler version doesn't recognize all the pragmas
+    #pragma GCC diagnostic ignored "-Wpragmas"
+    // Turn off some warnings
+    #pragma GCC diagnostic ignored "-Wdeprecated-copy"
+  #endif
+
+  // Clang:
+  // icpc apparently also defines __clang__ 
+  #if defined(__clang__) && !defined(__ICC)
+    // Save diagnostic state
+    #pragma clang diagnostic push 
+    // Don't care if an old compiler version doesn't recognize all the pragmas
+    #pragma clang diagnostic ignored "-Wpragmas"
+    // Turn off some warnings
+    #pragma clang diagnostic ignored "-Wdeprecated-copy"
+  #endif
+
+#endif
