90336460be7cdd212e13ac14c9b1ce7458d580c3
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2023-03-01 19:52:14 +0100

    Made the flav_prediction type printable with the cout and ascii printers. Added "::central" and "::covariance" tags to more clearly distinguish the two types of datasets coming from a flav_prediction instance.

--- Printers/include/gambit/Printers/printers/asciitypes.hpp
+++ Printers/include/gambit/Printers/printers/asciitypes.hpp
@@ -25,6 +25,9 @@
   (std::string)                             \
   (triplet<double>)                         \
   (map_intpair_dbl)                         \
+  (map_const_str_dbl)                       \
+  (map_const_str_map_const_str_dbl)         \
+  (flav_prediction)                         \
 
 
 #define ASCII_BACKEND_TYPES                 \
--- Printers/src/printers/asciiprinter/print_overloads.cpp
+++ Printers/src/printers/asciiprinter/print_overloads.cpp
@@ -136,6 +136,9 @@ namespace Gambit
     // Piggyback off existing print functions to build standard overloads
     USE_COMMON_PRINT_OVERLOAD(asciiPrinter, ModelParameters)
     USE_COMMON_PRINT_OVERLOAD(asciiPrinter, triplet<double>)
+    USE_COMMON_PRINT_OVERLOAD(asciiPrinter, map_const_str_dbl)
+    USE_COMMON_PRINT_OVERLOAD(asciiPrinter, map_const_str_map_const_str_dbl)
+    USE_COMMON_PRINT_OVERLOAD(asciiPrinter, flav_prediction)
     #ifndef SCANNER_STANDALONE
       USE_COMMON_PRINT_OVERLOAD(asciiPrinter, DM_nucleon_couplings)
       USE_COMMON_PRINT_OVERLOAD(asciiPrinter, BBN_container)
